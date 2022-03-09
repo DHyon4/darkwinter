9e51f8b445e86e4efb27923f9a496b0ebb765591
Edward Forgacs <eddie.forgacs@gmail.com>
2022-03-09 09:47:24 -0500

    Initial commits

---
+++ darkwinter.py
@@ -0,0 +1,8 @@
+
+
+def play():
+    inventory = []
+    print("Dark Winter")
+    print("Snow is falling softly onto the lake shore. You are standing next to a small boat.")
+    while True:
+        action_input = get_player_command()
